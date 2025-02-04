e38d62919d3177c6671c21d90b615dd6441df8a3
Jila06 <jbrown@vlacademy>
2025-02-03 20:39:55 -0600

    Hicimos la pagina practica calculadora

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -2,8 +2,32 @@
 console.log(`Funcionando Archivo: Practica_Calculadora`);
 
  function enviarDatos() {
+    debugger
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
-
+    
+    //Mostrar
+    //Alert , Console log /Inner 
+    //cl
+    console.log('Mostrar Nombre :',nombre);
+    console.log('Mostrar Edad:',edad);
+    alert(`Nombre: ${nombre} Edad: ${edad}`)
 }
 
+function realizarSuma() {
+    console.log("Funcionando realizarSuma")
+
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+
+//LS , LO , LA
+   //Typescript int decimal
+   //ParseInt 
+    let Suma = parseInt(numero1) + parseInt(numero2)
+
+    document.getElementById('resultado').innerText = Suma;
+
+    console.log(numero1)
+    console.log(numero2)
+    console.log(Suma) 
+}
