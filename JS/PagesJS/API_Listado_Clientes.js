a99ee48db7170596859a066a7998ed7fa0c21ec1
Jila06 <jbrown@vlacademy>
2025-02-10 20:25:03 -0600

    Puntos Extra

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -5,7 +5,7 @@ const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 // Datos locales simulando la API
 let Datos_Locales = [];
 
-DatosFiltrados = []
+let DatosFiltrados = []
 let contenedor = document.getElementById('tbody');//el contenedor se puso afuera para reutilizarlo
 
 DatosFiltrados = Datos_Locales
@@ -13,12 +13,27 @@ DatosFiltrados = Datos_Locales
 // Mostrar_CampoTabla()
 document.addEventListener('DOMContentLoaded', () => {
     consultarDatos()
-    
+    ConsultarAPIClientes()
+
 });
 
 
 function consultarDatos() {
 
+    fetch(Api_Usuarios)
+        
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => {
+            // common error
+            return null;
+        });
+    
+}
+
+function ConsultarAPIClientes() {
     fetch(Api_Usuarios)
         
         .then(response => response.json())
