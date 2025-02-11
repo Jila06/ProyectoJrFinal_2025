fbfb1f395e36b16e9d8cbab65cb48a46fb0c26ca
Jila06 <jbrown@vlacademy>
2025-02-10 20:41:13 -0600

    Error

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -3,46 +3,76 @@ console.log('Funcionando Archivo: API_Listado_Clientes');
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
 // Datos locales simulando la API
-let Datos_Locales = [];
+// Datos locales simulando la API
+
+
 
 let DatosFiltrados = []
-let contenedor = document.getElementById('tbody');//el contenedor se puso afuera para reutilizarlo
+let contenedor = document.getElementById('tbody');
 
-DatosFiltrados = Datos_Locales
 
-// Mostrar_CampoTabla()
+//aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
     consultarDatos()
-    ConsultarAPIClientes()
+    // ConsultarAPIClientes()
 
 });
 
+ // Consultar Datos del API mediante Fetch(AJAX) AXIOS
 
 function consultarDatos() {
-
-    fetch(Api_Usuarios)
+    //fetchget
+    fetch(Api_Usuarios) //1- se le indica la url de la api 
         
-        .then(response => response.json())
-        .then(result => {
+        .then(response => response.json()) // 2 -Conversion de datos , JSON 
+        .then(result => {           //3- Resultado  result
+            // custom error
             console.log(result)
+            DatosFiltrados = result
         })
-        .catch(error => {
+        .catch(error => {      ///4- Catch
             // common error
+            alert("Error ",error)
             return null;
         });
     
 }
 
-function ConsultarAPIClientes() {
-    fetch(Api_Usuarios)
-        
-        .then(response => response.json())
-        .then(result => {
-            console.log(result)
-        })
-        .catch(error => {
-            // common error
-            return null;
-        });
+
+function Mostrar_CampoTabla() {
     
+    let html = ""
+
+    DatosFiltrados.forEach(element => {
+        console.log(element)
+
+        html += `
+        <tr>
+            <td>${element.id}</td>
+            <td>${element.name} </td>
+            <td>${element.email}</td>
+        </tr>
+        
+        
+        
+        `
+        
+    });
+
+
+    contenedor.innerHTML = html
 }
+
+// function ConsultarAPIClientes() {
+//     fetch(Api_Usuarios)
+        
+//         .then(response => response.json())
+//         .then(result => {
+//             console.log(result)
+//         })
+//         .catch(error => {
+//             // common error
+//             return null;
+//         });
+    
+// }
