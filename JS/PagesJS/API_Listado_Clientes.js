2c164bd197a396aaeaf0b5fff19639de30b666e5
Jila06 <jbrown@vlacademy>
2025-02-10 20:16:34 -0600

    Error

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -1,3 +1,33 @@
 console.log('Funcionando Archivo: API_Listado_Clientes');
 
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
+
+// Datos locales simulando la API
+let Datos_Locales = [];
+
+DatosFiltrados = []
+let contenedor = document.getElementById('tbody');//el contenedor se puso afuera para reutilizarlo
+
+DatosFiltrados = Datos_Locales
+
+// Mostrar_CampoTabla()
+document.addEventListener('DOMContentLoaded', () => {
+    consultarDatos()
+    
+});
+
+
+function consultarDatos() {
+
+    fetch(Api_Usuarios)
+        
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => {
+            // common error
+            return null;
+        });
+    
+}
