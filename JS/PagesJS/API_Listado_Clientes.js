18e7ef4b80c8108db8d2d63dad34d3dfa7332c48
Jila06 <jbrown@vlacademy>
2025-02-11 21:11:00 -0600

    Entregable 5 proyecto

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -4,6 +4,8 @@ const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
 const Api2 = "https://rickandmortyapi.com/api/character"
 
+const Api3 = "https://jsonplaceholder.typicode.com/comments";
+
 // Datos locales simulando la API
 // Datos locales simulando la API
 
@@ -11,20 +13,23 @@ const Api2 = "https://rickandmortyapi.com/api/character"
 
 let DatosFiltrados = []
 let DatosFiltrados2 = []
+let DatosFiltrados3 = [];
+
 let contenedor = document.getElementById('tbody');
-let contenedor2 = document.getElementById('id');
+let contenedor2 = document.getElementById('tbody2');
+let contenedor3 = document.getElementById('tbody3');
 
 //aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
     consultarDatos()
     // ConsultarAPIClientes()
     consultarDatos2()
+    consultarDatos3()
 
 });
 
- 
 
- 
+
 function consultarDatos() {
     //fetchget
     fetch(Api_Usuarios) //1- se le indica la url de la api 
@@ -45,7 +50,6 @@ function consultarDatos() {
     
 }
 
-
 function consultarDatos2() {
     //fetchget
     fetch(Api2) //1- se le indica la url de la api 
@@ -54,8 +58,27 @@ function consultarDatos2() {
         .then(result => {           //3- Resultado  result
             // custom error
             console.log(result.results)
-            
+            DatosFiltrados2 = result.results
+            Mostrar_CampoTabla2()
+        })
+        .catch(error => {      ///4- Catch
+            // common error
+            alert("Error ",error)
+            return null;
+        });
+    
+}
 
+function consultarDatos3() {
+    //fetchget
+    fetch(Api3) //1- se le indica la url de la api 
+        
+        .then(response => response.json()) // 2 -Conversion de datos , JSON 
+        .then(result => {           //3- Resultado  result
+            // custom error
+            console.log(result)
+            DatosFiltrados3 = result
+            Mostrar_CampoTabla3()
         })
         .catch(error => {      ///4- Catch
             // common error
@@ -88,6 +111,59 @@ function Mostrar_CampoTabla() {
 
 
     contenedor.innerHTML = html
+}
+
+
+
+function Mostrar_CampoTabla2() {
+    
+    let html = ""
+    
+    DatosFiltrados2.forEach(element => {
+        
+        console.log(element)
+        html += `
+        
+         <tr>
+            <td> ${element.id} </td>
+            <td> ${element.name} </td>
+            <td> ${element.status} </td>
+            <td> ${element.species} </td>
+            <td> <img src="${element.image}" alt="">  </td>
+        
+        </tr>
+        
+        
+        `
+    });
+
+    contenedor2.innerHTML = html
+}
+
+
+function Mostrar_CampoTabla3() {
+    
+    let html = ""
+    
+    DatosFiltrados3.forEach(element => {
+        
+        console.log(element)
+        html += `
+        
+         <tr>
+            <td> ${element.id} </td>
+            <td> ${element.name} </td>
+            <td> ${element.email} </td>
+            <td> ${element.body} </td>
+            
+        
+        </tr>
+        
+        
+        `
+    });
+
+    contenedor3.innerHTML = html
 }
 
 // function ConsultarAPIClientes() {
